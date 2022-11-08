4ac15bb0072c36a630f910f97cbebe69f1993271
mz4 <matteozaratti@gmail.com>
2022-11-08 15:56:44 +0000

    Add new user side panel

--- src/app/library/button/Button.js
+++ src/app/library/button/Button.js
@@ -1,8 +1,12 @@
 import styles from "./Button.module.scss";
 
-export const Button = ({ actionButton, text }) => {
+export const Button = ({ actionButton, text, type }) => {
+  const buttonType = {
+    'primary': styles.primary,
+    'secondary': styles.secondary
+  }
   return (
-    <button onClick={actionButton} className={styles.primaryButton}>
+    <button onClick={actionButton} className={buttonType[type]}>
       {text}
     </button>
   );
