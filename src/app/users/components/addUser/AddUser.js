8b23dd889e339bdf4a6247c4b099ef37b5ed309b
Matteo <matteozaratti@gmail.com>
2022-11-12 15:59:32 +0100

    Add close button to modal

---
+++ src/app/users/components/addUser/AddUser.js
@@ -0,0 +1,4 @@
+const AddUser = ({ user, showProfileDetails }) => {
+  return <div>AAAA</div>;
+};
+export default AddUser;
--- src/app/users/components/index.js
+++ src/app/users/components/index.js
@@ -2,5 +2,6 @@ import Details from './details/Details';
 import Header from './header/Header';
 import User from './user/User';
 import Users from './users/Users';
+import AddUser from './addUser/AddUser';
 
-export { Details, Header, User, Users };
+export { Details, Header, User, Users, AddUser };
