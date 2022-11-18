9acfb0ac09a63305bce92ff778a4e6d7837b3efb
Matteo <matteozaratti@gmail.com>
2022-11-18 18:44:37 +0100

    Fix useFetch tests

--- src/app/hooks/useFetch.test.js
+++ src/app/hooks/useFetch.test.js
@@ -4,19 +4,25 @@ import { useFetch } from './useFetch';
 
 jest.mock('../service/request');
 
+const user = {
+  id: 1,
+  email: 'george.bluth@reqres.in',
+  first_name: 'Georgie',
+  last_name: 'Bluth',
+  avatar: 'https://reqres.in/img/faces/1-image.jpg'
+};
+
 describe('Test useFetch custom hook', () => {
   const url = 'http://localhost:3001/users';
   afterEach(() => jest.resetAllMocks());
   it('Return data from api', async () => {
-    const mockGet = jest.fn().mockResolvedValue([{ id: 1 }]);
-    Request.prototype.get = await mockGet();
+    Request.prototype.get = jest.fn().mockResolvedValue([user]);
     const { result, waitForNextUpdate } = renderHook(() => useFetch(url));
     expect(result.current.data).toEqual([]);
     act(() => {
       result.current.getData(url);
     });
     await waitForNextUpdate();
-    const ao = result.current.data;
-    expect(ao).toEqual([]);
+    expect(result.current.data).toEqual([user]);
   });
 });
