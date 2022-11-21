fbc4059e8e693937dca75bb4db1720728cd79e01
Matteo <matteozaratti@gmail.com>
2022-11-21 15:21:29 +0100

    Add tests to add user form

--- src/app/users/components/addUser/AddUser.test.js
+++ src/app/users/components/addUser/AddUser.test.js
@@ -10,7 +10,7 @@ import AddUser from './AddUser';
 const submit = jest.fn();
 
 describe('test addUser form', () => {
-  it('test add user', async () => {
+  test('Submit is not called if form is not filled', async () => {
     render(<AddUser submit={submit} />);
     const element = screen.getByTestId('submitForm');
     act(() => {
@@ -18,4 +18,19 @@ describe('test addUser form', () => {
     });
     await waitFor(() => expect(submit).not.toHaveBeenCalled());
   });
+});
+
+test('Submit is called if form is complete without errors', async () => {
+  render(<AddUser submit={submit} />);
+  const element = screen.getByTestId('submitForm');
+  const email = screen.getByPlaceholderText('email');
+  const firstName = screen.getByPlaceholderText('First Name');
+  const lastName = screen.getByPlaceholderText('Last Name');
+  fireEvent.change(email, { target: { value: 'emailx@gmail.com' } });
+  fireEvent.change(firstName, { target: { value: 'John' } });
+  fireEvent.change(lastName, { target: { value: 'White' } });
+  act(() => {
+    fireEvent.click(element);
+  });
+  await waitFor(() => expect(submit).toHaveBeenCalled());
 });
