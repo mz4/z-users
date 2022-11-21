911250c5bf3de98a16687d63cd893944d266241a
Matteo <matteozaratti@gmail.com>
2022-11-21 14:35:23 +0100

    Add tests to user

--- src/app/users/components/user/User.test.js
+++ src/app/users/components/user/User.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import User from './User';
 
 const user = {
@@ -14,4 +14,11 @@ it('Render a single user', () => {
   render(<User user={user} showProfileDetails={showProfileDetails} />);
   const userText = screen.getAllByText('Mark Guy');
   expect(userText.length).toBe(1);
+});
+
+it('Trigger show profile details', () => {
+  render(<User user={user} showProfileDetails={showProfileDetails} />);
+  const element = screen.getByTestId('userDetails');
+  fireEvent.click(element);
+  expect(showProfileDetails).toHaveBeenCalledTimes(1);
 });
