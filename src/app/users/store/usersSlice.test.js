2677322331d897014b88d07d31b212288142f98c
Matteo <matteozaratti@gmail.com>
2022-11-23 13:23:36 +0100

    Add action test

--- src/app/users/store/usersSlice.test.js
+++ src/app/users/store/usersSlice.test.js
@@ -1,8 +1,16 @@
 import store from '../../store/store';
+import { usersList } from './usersSlice';
+import { users } from '../../data/mockData';
 
 describe('initial state', () => {
   it('should initially set users', () => {
     const state = store.getState().users;
     expect(state.users).toEqual([]);
+  });
+
+  it('should set users', () => {
+    store.dispatch(usersList(users));
+    const state = store.getState().users;
+    expect(state.users).toEqual(users);
   });
 });
