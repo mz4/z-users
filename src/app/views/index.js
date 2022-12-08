252d63e98cf6b0e8d1fe3873bf1c117ed2d64545
Matteo <matteozaratti@gmail.com>
2022-12-08 21:43:49 +0100

    Add login

--- src/app/views/index.js
+++ src/app/views/index.js
@@ -6,10 +6,11 @@ import Users from './users/containers/usersManagement/UsersManagement';
 
 export const Views = () => {
   const isAuth = useSelector(isAuthenticated);
+  console.log('isAuth: ', isAuth);
   return (
     <Routes>
       <Route exact path="/" element={<Navigate replace to="/login" />} />
-      <Route exact path="/login" element={<Login />} />
+      <Route exact path="/login" element={isAuth ? <Users /> : <Login />} />
       <Route exact path="/users" element={isAuth ? <Users /> : <Login />} />
     </Routes>
   );
