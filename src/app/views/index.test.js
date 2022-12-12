f77e8750f059286bb309fa96376be891313cd219
Matteo <matteozaratti@gmail.com>
2022-12-12 18:56:37 +0100

    Add tests to app and index

--- src/app/views/index.test.js
+++ src/app/views/index.test.js
@@ -1,8 +1,9 @@
+import { BrowserRouter as Router } from 'react-router-dom';
 import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import configureStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
-import App from './App';
+import Views from './index';
 
 const middlewares = [thunk];
 const mockStore = configureStore(middlewares);
@@ -28,10 +29,13 @@ describe('test app', () => {
   };
   it('renders without crashing', () => {
     setup();
-    render(
+    const { debug } = render(
       <Provider store={store}>
-        <App />
+        <Router>
+          <Views />
+        </Router>
       </Provider>
     );
+    console.log(debug());
   });
 });
